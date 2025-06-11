db9ee9c234fcd5580810b1ddd1634fa55b596bc4
jhow4b <103279592+jhow4b@users.noreply.github.com>
2025-06-11 14:36:25 -0300

    Atualização de estilos e Footer

---
+++ script.js
@@ -0,0 +1,14 @@
+const video = document.querySelector(".thumbnail")
+
+video.addEventListener("click", () => {
+    video.innerHTML = `
+    <iframe
+        width="100%"
+        height="500px"
+        src="https://www.youtube.com/embed/NiWE4Vfv0k8?autoplay=1&si=kGOtjv4bcV88Sdi4"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+    ></iframe>
+    `;
+});
