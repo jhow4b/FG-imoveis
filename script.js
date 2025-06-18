dfa06214b433e476e79a1ff15a5e2f6e7305fa22
jhow4b <103279592+jhow4b@users.noreply.github.com>
2025-06-18 13:22:45 -0300

    video atualizado

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ video.addEventListener("click", () => {
     <iframe
         width="100%"
         height="500px"
-        src="https://www.youtube.com/embed/NiWE4Vfv0k8?autoplay=1&si=kGOtjv4bcV88Sdi4"
+        src="https://www.youtube.com/embed/pRfgmUVvFbU?si=LdmbgbWYpCde85bb"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowfullscreen
