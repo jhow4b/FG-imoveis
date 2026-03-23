cc28f7205ac83e54ff6a41a8f527362747b32819
jhow4b <103279592+jhow4b@users.noreply.github.com>
2026-03-23 20:56:28 -0300

    att

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const video = document.querySelector(".thumbnail")
+const video = document.querySelector(".thumbnail");
 
 video.addEventListener("click", () => {
-    video.innerHTML = `
-    <iframe
-        width="100%"
-        height="500px"
-        src="https://www.youtube.com/embed/pRfgmUVvFbU?si=LdmbgbWYpCde85bb&autoplay=1"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-    ></iframe>
+  video.innerHTML = `
+        <iframe
+            width="100%"
+            height="500"
+            src="https://www.youtube.com/embed/l7TYxo0Ga-k?autoplay=1"
+            frameborder="0"
+            allow="autoplay; encrypted-media"
+            allowfullscreen
+        ></iframe>
     `;
 });
 
 function openWeb(url) {
-    window.open(url, "_blank")
+  window.open(url, "_blank");
 }
